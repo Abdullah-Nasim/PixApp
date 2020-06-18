f6938e7179c59d248316cbda359cabaef4668f13
Abdullah Nasim <abdullah@idealz.com>
2020-06-18 20:38:44 +0400

    Initial Commit

---
+++ app/src/main/java/com/android/pixapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.pixapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
